e42d748b2b95bd8670ac519ff2f2201049698860
gklsndr <gkl.sundar@gmail.com>
2022-04-30 02:18:23 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { render } from "react-dom";
+import Chart from "./Chart";
+import { getData } from "./utils";
+
+class ChartComponent extends React.Component {
+  componentDidMount() {
+    getData().then((data) => {
+      this.setState({ data });
+    });
+  }
+  render() {
+    if (this.state == null) {
+      return <div>Loading...</div>;
+    }
+    return <Chart data={this.state.data} />;
+  }
+}
+
+render(<ChartComponent />, document.getElementById("root"));

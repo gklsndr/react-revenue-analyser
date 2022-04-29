6da72232d3222d5a7f89e678fcd44ec9b3810026
gklsndr <gkl.sundar@gmail.com>
2022-04-30 04:03:48 +0530

    Chart + Table

---
+++ src/Table.js
@@ -0,0 +1,209 @@
+import React from "react";
+import clsx from "clsx";
+import { Theme, styled } from "@mui/material/styles";
+import TableCell from "@mui/material/TableCell";
+import Paper from "@mui/material/Paper";
+import {
+  AutoSizer,
+  Column,
+  Table,
+  TableCellRenderer,
+  TableHeaderProps
+} from "react-virtualized";
+
+const classes = {
+  flexContainer: "ReactVirtualizedDemo-flexContainer",
+  tableRow: "ReactVirtualizedDemo-tableRow",
+  tableRowHover: "ReactVirtualizedDemo-tableRowHover",
+  tableCell: "ReactVirtualizedDemo-tableCell",
+  noClick: "ReactVirtualizedDemo-noClick"
+};
+
+const styles = ({ theme }) => ({
+  // temporary right-to-left patch, waiting for
+  // https://github.com/bvaughn/react-virtualized/issues/454
+  "& .ReactVirtualized__Table__headerRow": {
+    ...(theme.direction === "rtl" && {
+      paddingLeft: "0 !important"
+    }),
+    ...(theme.direction !== "rtl" && {
+      paddingRight: undefined
+    })
+  },
+  [`& .${classes.flexContainer}`]: {
+    display: "flex",
+    alignItems: "center",
+    boxSizing: "border-box"
+  },
+  [`& .${classes.tableRow}`]: {
+    cursor: "pointer"
+  },
+  [`& .${classes.tableRowHover}`]: {
+    "&:hover": {
+      backgroundColor: theme.palette.grey[200]
+    }
+  },
+  [`& .${classes.tableCell}`]: {
+    flex: 1
+  },
+  [`& .${classes.noClick}`]: {
+    cursor: "initial"
+  }
+});
+
+class MuiVirtualizedTable extends React.PureComponent {
+  static defaultProps = {
+    headerHeight: 48,
+    rowHeight: 48
+  };
+
+  getRowClassName = ({ index }) => {
+    const { onRowClick } = this.props;
+
+    return clsx(classes.tableRow, classes.flexContainer, {
+      [classes.tableRowHover]: index !== -1 && onRowClick != null
+    });
+  };
+
+  cellRenderer = ({ cellData, columnIndex }) => {
+    const { columns, rowHeight, onRowClick } = this.props;
+    return (
+      <TableCell
+        component="div"
+        className={clsx(classes.tableCell, classes.flexContainer, {
+          [classes.noClick]: onRowClick == null
+        })}
+        variant="body"
+        style={{ height: rowHeight }}
+        align={
+          (columnIndex != null && columns[columnIndex].numeric) || false
+            ? "right"
+            : "left"
+        }
+      >
+        {cellData}
+      </TableCell>
+    );
+  };
+
+  headerRenderer = ({ label, columnIndex }) => {
+    const { headerHeight, columns } = this.props;
+
+    return (
+      <TableCell
+        component="div"
+        className={clsx(
+          classes.tableCell,
+          classes.flexContainer,
+          classes.noClick
+        )}
+        variant="head"
+        style={{ height: headerHeight }}
+        align={columns[columnIndex].numeric || false ? "right" : "left"}
+      >
+        <span>{label}</span>
+      </TableCell>
+    );
+  };
+
+  render() {
+    const { columns, rowHeight, headerHeight, ...tableProps } = this.props;
+    return (
+      <AutoSizer>
+        {({ height, width }) => (
+          <Table
+            height={height}
+            width={width}
+            rowHeight={rowHeight}
+            gridStyle={{
+              direction: "inherit"
+            }}
+            headerHeight={headerHeight}
+            {...tableProps}
+            rowClassName={this.getRowClassName}
+          >
+            {columns.map(({ dataKey, ...other }, index) => {
+              return (
+                <Column
+                  key={dataKey}
+                  headerRenderer={(headerProps) =>
+                    this.headerRenderer({
+                      ...headerProps,
+                      columnIndex: index
+                    })
+                  }
+                  className={classes.flexContainer}
+                  cellRenderer={this.cellRenderer}
+                  dataKey={dataKey}
+                  {...other}
+                />
+              );
+            })}
+          </Table>
+        )}
+      </AutoSizer>
+    );
+  }
+}
+
+const VirtualizedTable = styled(MuiVirtualizedTable)(styles);
+
+export default function ReactVirtualizedTable(tableData) {
+  return (
+    <Paper style={{ height: 400, width: "100%" }}>
+      <VirtualizedTable
+        rowCount={tableData.length}
+        rowGetter={({ index }) => tableData[index]}
+        columns={[
+          {
+            // width: 10,
+            label: "S.No",
+            dataKey: "S_no"
+          },
+          {
+            width: 200,
+            label: "Line of Business",
+            dataKey: "line_of_business",
+            numeric: true
+          },
+          {
+            width: 200,
+            label: "Revenue Type",
+            dataKey: "revenue_type",
+            numeric: true
+          },
+          {
+            width: 200,
+            label: "Product",
+            dataKey: "product",
+            numeric: true
+          },
+          {
+            width: 200,
+            label: "Posting Period",
+            dataKey: "date",
+            numeric: true
+          },
+          {
+            width: 120,
+            label: "ACV",
+            dataKey: "acv",
+            numeric: true
+          },
+          {
+            width: 120,
+            label: "TCV",
+            dataKey: "tcv",
+            numeric: true
+          },
+          {
+            width: 120,
+            label: "Revenue",
+            dataKey: "revenue",
+            numeric: true
+          }
+        ]}
+      />
+    </Paper>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,18 +2,69 @@ import React from "react";
 import { render } from "react-dom";
 import Chart from "./Chart";
 import { getData } from "./utils";
+import MenuItem from "@mui/material/MenuItem";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import revenueData from "../public/data.js";
+import "../public/styles.css";
+import ReactVirtualizedTable from "./Table";
 
 class ChartComponent extends React.Component {
   componentDidMount() {
     getData().then((data) => {
       this.setState({ data });
+      console.log(data);
     });
   }
   render() {
-    if (this.state == null) {
-      return <div>Loading...</div>;
+    const chartTypes = [
+      "Revenue & ACV timeline",
+      "Revenue & TCV timeline",
+      "ACV vs TCV"
+    ];
+    this.selectedChart = chartTypes[0];
+    const ITEM_HEIGHT = 48;
+    const ITEM_PADDING_TOP = 8;
+    const MenuProps = {
+      PaperProps: {
+        style: {
+          maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+          width: 250
+        }
+      }
+    };
+
+    function handleChange(eventData) {
+      this.selectedChart = eventData.target.value;
+      console.log("Chart Selected", eventData.target.value);
     }
-    return <Chart data={this.state.data} />;
+
+    return (
+      <div className="appRoot">
+        <div className="header">
+          <Select
+            value={this.selectedChart}
+            onChange={handleChange}
+            MenuProps={MenuProps}
+            inputProps={{ "aria-label": "Without label" }}
+          >
+            {chartTypes.map((name) => (
+              <MenuItem key={name} value={name} classes="chartItem">
+                {name}
+              </MenuItem>
+            ))}
+          </Select>
+          Hi Bruce Wayne
+        </div>
+        {!this.state && <div>Loading...</div>}
+
+        {this.state && (
+          <div>
+            <Chart data={this.state.data} />
+            {ReactVirtualizedTable(revenueData)}
+          </div>
+        )}
+      </div>
+    );
   }
 }
 
